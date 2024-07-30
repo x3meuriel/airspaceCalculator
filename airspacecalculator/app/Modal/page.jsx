32e74750ccf8bc07bc35bedbf43125f942bdce41
x3meuriel <x3me7@hotmail.com>
2024-07-30 09:16:18 +0100

    chore: Update import path for Estimate component in page.jsx

--- airspacecalculator/app/Modal/page.jsx
+++ airspacecalculator/app/Modal/page.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import axios from 'axios';
-import { Estimate } from '../Estimate/page';
+import Estimate from '../Estimate/page';
 
 function Modal() {
   const [address, setAddress] = React.useState('');
